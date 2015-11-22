cc4f71ccadc0867ac7e3b2307d8da24b3c338dab
chkfj <keita.imaeda@gmail.com>
2015-11-22 13:05:54 +0900

    add supervisord configs

---
+++ runinenv.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+VENV=$1
+echo $VENV
+if [ -z $VENV ]; then
+echo "usage: runinenv [virtualenv_path] CMDS"
+  exit 1
+fi
+. ${VENV}/bin/activate
+shift 1
+echo "Executing $@ in ${VENV}"exec "$@"
+

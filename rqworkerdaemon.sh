549f6783f3f3ac725e80c4785b9dbee9b4d8338c
chkfj <keita.imaeda@gmail.com>
2015-11-22 10:41:36 +0900

    add init.d script

---
+++ rqworkerdaemon.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export ENV_NAME=encode
+export VIRTUALENV_PATH=/home/chinachu/env/$ENV_NAME
+export PYTHON=$VIRTUALENV_PATH/bin/python
+
+cd $(dirname $0)
+${VIRTUALENV_PATH}/bin/rqworker
+

98bcd88c5406e2db0b70889475a6977ea23da433
chkfj <keita.imaeda@gmail.com>
2015-11-22 10:32:07 +0900

    no encode when mp4 already exists

--- encode.py
+++ encode.py
@@ -4,9 +4,10 @@ import json
 import os
 import sys
 
-def encodejob(ts):
+def encodejob(ts, force=False):
     dst_mp4 = "/media/hdd/encoded/" + os.path.basename(ts).split(".")[0] + ".mp4"
     encode_command = "ffmpeg -y -i %s -strict -2 -vcodec libx264 -r 30000/1001 -aspect 16:9 -s 1920x1080 -bufsize 20000k -minrate 2000k -pass 1 -movflags faststart -threads 2 %s" % (ts, dst_mp4)
-    os.system(encode_command)
-    
+
+    if not os.path.exists(dst_mp4) or force:
+        os.system(encode_command)
 

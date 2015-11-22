83149baa28f0170676565d4dcc3f37a4a866d032
chkfj <keita.imaeda@gmail.com>
2015-11-22 20:30:36 +0900

    add settings.py

--- encode.py
+++ encode.py
@@ -1,11 +1,12 @@
 # coding: utf-8
 
+import settings
 import json
 import os
 import sys
 
 def encodejob(ts, force=False):
-    dst_mp4 = "/media/hdd/encoded/" + os.path.basename(ts).split(".")[0] + ".mp4"
+    dst_mp4 = settings["encoded_dir"] + "/" + os.path.basename(ts).split(".")[0] + ".mp4"
     encode_command = "ffmpeg -y -i %s -strict -2 -vcodec libx264 -r 30000/1001 -aspect 16:9 -s 1920x1080 -bufsize 20000k -minrate 2000k -pass 1 -movflags faststart -threads 2 %s" % (ts, dst_mp4)
 
     if not os.path.exists(dst_mp4) or force:
--- thumbnail.py
+++ thumbnail.py
@@ -1,9 +1,10 @@
 #coding: utf-8
 
+import settings
 import os
 
 def thumbnailjob(ts):
-    thumb_jpg = "/media/hdd/encoded/" + os.path.basename(ts).split(".")[0] + ".mp4.cover.jpg"
+    thumb_jpg = settings["encoded_dir"] + "/" + os.path.basename(ts).split(".")[0] + ".mp4.cover.jpg"
     command = "ffmpeg -y -i %s -ss 120 -vframes 1 -f image2 -s 720x480 %s" % (ts, thumb_jpg)
     os.system(command)
 

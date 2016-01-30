658eca2d024bbafe0bf5479743522af32203ccc8
chkfj <keita.imaeda@gmail.com>
2016-01-30 14:44:54 +0900

    fix #12

--- encode.py
+++ encode.py
@@ -7,7 +7,7 @@ import sys
 
 def encodejob(ts, force=False):
     dst_mp4 = settings["encoded_dir"] + "/" + os.path.basename(ts).split(".")[0] + ".mp4"
-    encode_command = "ffmpeg -y -i %s -strict -2 -vcodec libx264 -r 30000/1001 -aspect 16:9 -s 1920x1080 -bufsize 20000k -minrate 2000k -pass 1 -movflags faststart -threads 2 %s" % (ts, dst_mp4)
+    encode_command = "ffmpeg -y -i %s -strict -2 -vcodec libx264 -flags +ilme+ildct -top -1 -deinterlace -r 30000/1001 -aspect 16:9 -s 1920x1080 -bufsize 20000k -minrate 2000k -pass 1 -movflags faststart -threads 2 %s" % (ts, dst_mp4)
 
     if not os.path.exists(dst_mp4) or force:
         os.system(encode_command)
